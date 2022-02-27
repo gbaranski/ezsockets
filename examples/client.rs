8ca4f6c168dc76570f911bcc27d72e0dc2d42c8a
Grzegorz Baranski <me@gbaranski.com>
2022-02-27 17:57:16 +0100

    chore: add examples/

---
+++ examples/client.rs
@@ -0,0 +1,49 @@
+use ezsockets::BoxError;
+use std::io::BufRead;
+use url::Url;
+
+struct Client {}
+
+use async_trait::async_trait;
+
+#[derive(Debug, thiserror::Error)]
+enum Error {}
+
+#[async_trait]
+impl ezsockets::Client for Client {
+    async fn text(&mut self, text: String) -> Result<Option<ezsockets::Message>, BoxError> {
+        println!("received message: {text}");
+        Ok(None)
+    }
+
+    async fn binary(&mut self, bytes: Vec<u8>) -> Result<Option<ezsockets::Message>, BoxError> {
+        println!("received bytes: {bytes:?}");
+        Ok(None)
+    }
+
+    async fn closed(
+        &mut self,
+        code: Option<ezsockets::CloseCode>,
+        reason: Option<String>,
+    ) -> Result<(), BoxError> {
+        println!("connection closed. close code: {code:#?}. reason: {reason:#?}");
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let client = Client {};
+    let url = Url::parse("ws://localhost:8080").unwrap();
+    let (handle, future) = ezsockets::connect(client, url).await;
+    tokio::spawn(async move {
+        future.await.unwrap();
+    });
+    let stdin = std::io::stdin();
+    let lines = stdin.lock().lines();
+    for line in lines {
+        let line = line.unwrap();
+        handle.text(line).await;
+    }
+}
---
+++ examples/server.rs
@@ -0,0 +1,99 @@
+use async_trait::async_trait;
+use ezsockets::BoxError;
+use ezsockets::ServerHandle;
+use ezsockets::SessionHandle;
+use std::collections::HashMap;
+
+type SessionID = u8;
+
+#[derive(Debug)]
+enum Message {
+    Broadcast { from: SessionID, text: String },
+}
+
+struct Server {
+    next_id: u8,
+    sessions: HashMap<u8, SessionHandle>,
+    handle: ServerHandle<Message>,
+}
+
+#[async_trait]
+impl ezsockets::Server<Session> for Server {
+    type Message = Message;
+
+    async fn connected(
+        &mut self,
+        handle: SessionHandle,
+        address: std::net::SocketAddr,
+    ) -> Result<Session, BoxError> {
+        let id = self.next_id;
+        self.next_id += 1;
+        tracing::info!("connected from {} with id {}", address, id);
+        self.sessions.insert(id, handle);
+        Ok(Session {
+            id,
+            server: self.handle.clone(),
+        })
+    }
+
+    async fn message(&mut self, message: Self::Message) {
+        match message {
+            Message::Broadcast { from, text } => {
+                let sessions = self.sessions.iter().filter(|(id, _)| **id != from);
+                for (id, handle) in sessions {
+                    println!("broadcasting {text} to {id}");
+                    handle.text(text.clone()).await;
+                }
+            }
+        };
+    }
+}
+
+struct Session {
+    id: SessionID,
+    server: ServerHandle<Message>,
+}
+
+#[async_trait]
+impl ezsockets::Session for Session {
+    async fn text(&mut self, text: String) -> Result<Option<ezsockets::Message>, BoxError> {
+        self.server
+            .call(Message::Broadcast {
+                from: self.id,
+                text,
+            })
+            .await;
+        Ok(None)
+    }
+
+    async fn binary(&mut self, _bytes: Vec<u8>) -> Result<Option<ezsockets::Message>, BoxError> {
+        unimplemented!()
+    }
+
+    async fn closed(
+        &mut self,
+        code: Option<ezsockets::CloseCode>,
+        reason: Option<String>,
+    ) -> Result<(), BoxError> {
+        println!("connection closed. close code: {code:#?}. reason: {reason:#?}");
+        Ok(())
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+enum Error {}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let (_, future) = ezsockets::run(
+        |handle| Server {
+            sessions: HashMap::new(),
+            handle,
+            next_id: 0,
+        },
+        "127.0.0.1:8080",
+    )
+    .await;
+    future.await.unwrap();
+}
