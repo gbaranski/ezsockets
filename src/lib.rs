fdcd2b86e0a704177a77334e3be9dccf31109948
Grzegorz Baranski <me@gbaranski.com>
2022-03-23 20:17:46 +0100

    style: reoder reexports

--- src/lib.rs
+++ src/lib.rs
@@ -30,11 +30,11 @@ cfg_if::cfg_if! {
         mod server;
         mod session;
 
-        pub use server::ServerExt;
         pub use server::Server;
+        pub use server::ServerExt;
 
-        pub use session::SessionExt;
         pub use session::Session;
+        pub use session::SessionExt;
     }
 }
 
