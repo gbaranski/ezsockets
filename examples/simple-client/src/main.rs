052f38e712b71c83740de1037c28335f1cd8f835
Grzegorz Baranski <me@gbaranski.com>
2022-03-23 20:29:12 +0100

    add a very simple example of client

---
+++ examples/simple-client/src/main.rs
@@ -0,0 +1,54 @@
+use async_trait::async_trait;
+use ezsockets::BoxError;
+use ezsockets::ClientConfig;
+use std::io::BufRead;
+use url::Url;
+
+struct Client {}
+
+#[async_trait]
+impl ezsockets::ClientExt for Client {
+    type Message = ();
+
+    async fn text(&mut self, text: String) -> Result<(), BoxError> {
+        tracing::info!("received message: {text}");
+        Ok(())
+    }
+
+    async fn binary(&mut self, bytes: Vec<u8>) -> Result<(), BoxError> {
+        tracing::info!("received bytes: {bytes:?}");
+        Ok(())
+    }
+
+    async fn closed(&mut self) -> Result<(), BoxError> {
+        Ok(())
+    }
+
+    async fn call(&mut self, message: Self::Message) {
+        match message {
+            () => {}
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let url = std::env::args()
+        .next()
+        .expect("missing URL, launch with `<program> <URL>`");
+    let url = Url::parse(&url).unwrap();
+    let config = ClientConfig::new(url);
+    let (handle, future) = ezsockets::connect(|_| Client {}, config).await;
+    tokio::spawn(async move {
+        future.await.unwrap();
+    });
+
+    let stdin = std::io::stdin();
+    let lines = stdin.lock().lines();
+    for line in lines {
+        let line = line.unwrap();
+        tracing::info!("sending {line}");
+        handle.text(line).await;
+    }
+}
