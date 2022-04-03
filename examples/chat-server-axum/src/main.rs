3f3c24ded87d635303ba39491db21efc72c8e967
Grzegorz Baranski <me@gbaranski.com>
2022-04-03 23:12:54 +0200

    fix(examples/chat-server-axum): use u16 instaed u8

--- examples/chat-server-axum/src/main.rs
+++ examples/chat-server-axum/src/main.rs
@@ -11,7 +11,7 @@ use std::collections::HashMap;
 use std::io::BufRead;
 use std::net::SocketAddr;
 
-type SessionID = u8;
+type SessionID = u16;
 type Session = ezsockets::Session<SessionID, ()>;
 
 #[derive(Debug)]
