3b17c63abe493269276e2cf160c0d0d47b14c875
Grzegorz Baranski <me@gbaranski.com>
2022-04-03 23:02:06 +0200

    feat: make chat example more complex

--- examples/chat-server/src/main.rs
+++ examples/chat-server/src/main.rs
@@ -3,22 +3,29 @@ use ezsockets::Error;
 use ezsockets::Server;
 use ezsockets::Socket;
 use std::collections::HashMap;
-use std::io::BufRead;
 use std::net::SocketAddr;
+
+const DEFAULT_ROOM: &str = "main";
 
 type SessionID = u8;
 type Session = ezsockets::Session<SessionID, ()>;
 
 #[derive(Debug)]
 enum Message {
-    Broadcast {
+    Join {
+        id: SessionID,
+        room: String,
+    },
+    Send {
+        from: SessionID,
+        room: String,
         text: String,
-        exceptions: Vec<SessionID>,
     },
 }
 
 struct ChatServer {
     sessions: HashMap<SessionID, Session>,
+    rooms: HashMap<String, Vec<SessionID>>,
     handle: Server<Self>,
 }
 
@@ -38,11 +45,13 @@ impl ezsockets::ServerExt for ChatServer {
             |_handle| SessionActor {
                 id,
                 server: self.handle.clone(),
+                room: DEFAULT_ROOM.to_string(),
             },
             id,
             socket,
         );
         self.sessions.insert(id, session.clone());
+        self.rooms.get_mut(DEFAULT_ROOM).unwrap().push(id);
         Ok(session)
     }
 
@@ -51,19 +60,76 @@ impl ezsockets::ServerExt for ChatServer {
         id: <Self::Session as ezsockets::SessionExt>::ID,
     ) -> Result<(), Error> {
         assert!(self.sessions.remove(&id).is_some());
+
+        let (ids, n) = self
+            .rooms
+            .values_mut()
+            .find_map(|ids| {
+                if let Some(n) = ids.iter().position(|v| id == *v) {
+                    Some((ids, n))
+                } else {
+                    None
+                }
+            })
+            .expect("could not find session in any room");
+        ids.remove(n);
         Ok(())
     }
 
     async fn call(&mut self, params: Self::Params) -> Result<(), Error> {
         match params {
-            Message::Broadcast { exceptions, text } => {
-                let sessions = self
-                    .sessions
+            Message::Send { from, room, text } => {
+                let (ids, sessions): (Vec<SessionID>, Vec<&Session>) = self
+                    .rooms
+                    .get(&room)
+                    .unwrap()
                     .iter()
-                    .filter(|(id, _)| !exceptions.contains(id));
-                for (id, handle) in sessions {
-                    tracing::info!("broadcasting {text} to {id}");
-                    handle.text(text.clone()).await;
+                    .filter(|id| **id != from)
+                    .map(|id| (id, self.sessions.get(id).unwrap()))
+                    .unzip();
+
+                tracing::info!(
+                    "sending {text} to [{sessions}] at `{room}`",
+                    sessions = ids
+                        .iter()
+                        .map(|id| id.to_string())
+                        .collect::<Vec<_>>()
+                        .join(",")
+                );
+                for session in sessions {
+                    session.text(text.clone()).await;
+                }
+            }
+            Message::Join { id, room } => {
+                let (ids, n) = self
+                    .rooms
+                    .values_mut()
+                    .find_map(|ids| {
+                        if let Some(n) = ids.iter().position(|v| id == *v) {
+                            Some((ids, n))
+                        } else {
+                            None
+                        }
+                    })
+                    .expect("could not find session in any room");
+                ids.remove(n);
+                if let Some(ids) = self.rooms.get_mut(&room) {
+                    ids.push(id);
+                } else {
+                    self.rooms.insert(room.clone(), vec![id]);
+                }
+
+                let sessions = self
+                    .rooms
+                    .get(&room)
+                    .unwrap()
+                    .iter()
+                    .map(|id| self.sessions.get(id).unwrap());
+
+                for session in sessions {
+                    session
+                        .text(format!("User with ID: {id} just joined {room} room"))
+                        .await;
                 }
             }
         };
@@ -74,6 +140,7 @@ impl ezsockets::ServerExt for ChatServer {
 struct SessionActor {
     id: SessionID,
     server: Server<ChatServer>,
+    room: String,
 }
 
 #[async_trait]
@@ -88,12 +155,26 @@ impl ezsockets::SessionExt for SessionActor {
 
     async fn text(&mut self, text: String) -> Result<(), Error> {
         tracing::info!("received: {text}");
-        self.server
-            .call(Message::Broadcast {
-                exceptions: vec![self.id],
-                text,
-            })
-            .await;
+        if text.starts_with("/") {
+            let mut args = text.split_whitespace();
+            let command = args.next().unwrap();
+            if command == "/join" {
+                let room = args.next().expect("missing <room> argument").to_string();
+                tracing::info!("moving {} to {room}", self.id);
+                self.room = room.clone();
+                self.server.call(Message::Join { id: self.id, room }).await;
+            } else {
+                tracing::error!("unrecognized command: {text}");
+            }
+        } else {
+            self.server
+                .call(Message::Send {
+                    text,
+                    from: self.id,
+                    room: self.room.clone(),
+                })
+                .await;
+        }
         Ok(())
     }
 
@@ -112,26 +193,10 @@ async fn main() {
     tracing_subscriber::fmt::init();
     let (server, _) = Server::create(|handle| ChatServer {
         sessions: HashMap::new(),
+        rooms: HashMap::from_iter([(DEFAULT_ROOM.to_string(), vec![])]),
         handle,
     });
-    tokio::spawn({
-        let server = server.clone();
-        async move {
-            ezsockets::tungstenite::run(server, "127.0.0.1:8080", |_| async move { Ok(()) })
-                .await
-                .unwrap();
-        }
-    });
-
-    let stdin = std::io::stdin();
-    let lines = stdin.lock().lines();
-    for line in lines {
-        let line = line.unwrap();
-        server
-            .call(Message::Broadcast {
-                text: line,
-                exceptions: vec![],
-            })
-            .await;
-    }
+    ezsockets::tungstenite::run(server, "127.0.0.1:8080", |_| async move { Ok(()) })
+        .await
+        .unwrap();
 }
